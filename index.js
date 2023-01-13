185dce82e18d5510db8f659ad6f21a5449d4651b
Andrew Smith <meuzishun@gmail.com>
2023-01-13 15:36:25 -0500

    Add header in res

--- index.js
+++ index.js
@@ -9,15 +9,27 @@ const server = http.createServer((req, res) => {
     '/contact-me': 'html',
   };
 
+  const contentType = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+  };
+
+  // Don't worry about favicos for right now...
   if (path.extname(req.url) === '.ico') return;
 
   const page = req.url === '/' ? 'index' : req.url;
   const url = path.join(__dirname, 'public', `${page}.${fileExtension[page]}`);
 
-  // Don't worry about favicos for right now...
-
   fs.readFile(url, 'utf-8', (err, content) => {
-    if (err) throw err;
+    if (err) {
+      //TODO: handle 404 and 500 here
+      throw err;
+    }
+    res.writeHead(200, { 'Content-Type': contentType[path.extname(url)] });
     res.write(content);
     res.end();
   });
