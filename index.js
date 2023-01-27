ec181985c007fd0bff8f043dcfdac76d53b4f1e9
Andrew Smith <meuzishun@gmail.com>
2023-01-27 17:57:48 -0500

    Change options so index route is required

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public', { index: false }));
 
-//? Soo... this index route is NOT needed?
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+//* This route is only needed if index is set to false (see above)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'about.html'));
